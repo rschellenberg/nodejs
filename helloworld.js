2bba642b78998c7f00ad14a389516d68517d956e
rschellenberg <41592742+rschellenberg@users.noreply.github.com>
2024-10-10 17:08:00 -0400

    Add files via upload
    Initial code

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+var os = require("os");
+var host = os.hostname();
+//const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`${host}`);
+});
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
+
