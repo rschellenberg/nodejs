8286e081055988baa0e145e1790ebde4dbc54c6a
rschellenberg <ryan@theschellenbergs.com>
2024-10-15 11:29:21 -0400

    clean up project files

--- ecs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo ECS_CLUSTER=portfolioplus-cluster >> /etc/ecs/ecs.config
--- ig.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "internet_gateway" {
- vpc_id = aws_vpc.main.id
- tags = {
-   Name = "internet_gateway"
- }
-}
