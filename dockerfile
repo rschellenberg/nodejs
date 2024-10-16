5506348bc2680a688b06cc87588c345ed32eaaa6
rschellenberg <ryan@theschellenbergs.com>
2024-10-16 07:40:12 -0400

    Updated nodejs codes and confirmed dockerfiles

--- dockerfile
+++ dockerfile
@@ -1,12 +1,9 @@
 FROM node:18-alpine
 
 WORKDIR /usr/src/app
-
 COPY package*.json ./
 
 RUN npm install
-# If you are building your code for production
-# RUN npm ci --omit=dev
 
 COPY . .
 
--- node.js
+++ node.js
@@ -2,11 +2,11 @@ const { createServer } = require('node:http');
 var os = require("os");
 var host = os.hostname();
 //const hostname = '127.0.0.1';
-const port = 3000;
+const port = 80;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`${host}`);
+  res.end(`${host}:{port}/`);
 });
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
