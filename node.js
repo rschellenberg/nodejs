8b880e1f83e52b043978d800be6cd7c751944da4
rschellenberg <ryan@theschellenbergs.com>
2024-10-16 10:34:26 -0400

    Added in version number

--- node.js
+++ node.js
@@ -1,12 +1,12 @@
 const { createServer } = require('node:http');
 var os = require("os");
 var host = os.hostname();
-//const hostname = '127.0.0.1';
+const version = '3.01';
 const port = 80;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`${host}:${port}/`);
+  res.end(`This code is running on: ${host}:${port}/ Version: ${version}`);
 });
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
